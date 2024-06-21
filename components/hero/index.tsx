ad0dadeab74574999188e958867bf86bf9c65795
juancho48 <juancho48@gmail.com>
2024-06-21 14:34:40 -0400

    Changing the hero content

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -12,7 +12,8 @@ import {
   SlideshowSlide,
 } from '~/components/ui/slideshow';
 
-import SlideshowBG from './slideshow-bg-01.jpg';
+import SlideshowBG01 from './slideshow-bg-01.jpg';
+import SlideshowBG02 from './slideshow-bg-02.jpg';
 
 export const Hero = () => (
   <Slideshow>
@@ -25,13 +26,12 @@ export const Hero = () => (
             fill
             priority
             sizes="(max-width: 1536px) 100vw, 1536px"
-            src={SlideshowBG}
+            src={SlideshowBG01}
           />
           <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
-            <h2 className="text-5xl font-black lg:text-6xl">25% Off Sale</h2>
+            <h2 className="text-5xl font-black lg:text-6xl">Achieve ultimate performance</h2>
             <p className="max-w-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              With the new Drivers used by the pros, you can achieve the ultimate performance
             </p>
             <Button asChild className="w-fit">
               <a href="/#">Shop now</a>
@@ -40,15 +40,24 @@ export const Hero = () => (
         </div>
       </SlideshowSlide>
       <SlideshowSlide>
-        <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
-          <h2 className="text-5xl font-black lg:text-6xl">Great Deals</h2>
-          <p className="max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-          <Button asChild className="w-fit">
-            <a href="/#">Shop now</a>
-          </Button>
+        <div className="relative">
+          <Image
+            alt="Some Takomo irons placeholder image"
+            className="absolute -z-10 object-cover"
+            fill
+            
+            sizes="(max-width: 1536px) 100vw, 1536px"
+            src={SlideshowBG02}
+          />
+          <div className="flex flex-col gap-4  px-12 pb-48 pt-36">
+            <h2 className="text-5xl font-black lg:text-6xl">Great Deals</h2>
+            <p className="max-w-xl">
+              Here are some great deals
+            </p>
+            <Button asChild className="w-fit">
+              <a href="/#">Shop now</a>
+            </Button>
+          </div>
         </div>
       </SlideshowSlide>
       <SlideshowSlide>
